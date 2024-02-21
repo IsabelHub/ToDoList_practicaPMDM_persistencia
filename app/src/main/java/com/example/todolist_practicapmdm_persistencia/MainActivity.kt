761507799da26689da9b11063b0493909a99c2ca
Isabel <emailsdeisabel@gmail.com>
2024-02-21 13:18:15 +0100

    mainActivity commit version 2

--- app/src/main/java/com/example/todolist_practicapmdm_persistencia/MainActivity.kt
+++ app/src/main/java/com/example/todolist_practicapmdm_persistencia/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.todolist_practicapmdm_persistencia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolist_practicapmdm_persistencia.database.TareaDatabase
+import com.example.todolist_practicapmdm_persistencia.repository.TareaRepositorio
+import com.example.todolist_practicapmdm_persistencia.viewmodel.TareaViewModel
+import com.example.todolist_practicapmdm_persistencia.viewmodel.TareaViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tareaViewModel: TareaViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    private fun setupViewModel(){
+        val noteRepository = TareaRepositorio(TareaDatabase(this))
+        val viewModelProviderFactory = TareaViewModelFactory(application , noteRepository)
+       tareaViewModel = ViewModelProvider(this, viewModelProviderFactory)[TareaViewModel::class.java]
     }
 }
